08a8970ba9d932e6cb01d2ad4f452fbf209b9ead
Said Rehouni <srehouni@virtual-affairs.com>
2017-05-21 22:36:37 +0200

    Created JobList component

---
+++ TemperAssessment/JobsList/JobListStyle.swift
@@ -0,0 +1,13 @@
+//
+//  JobListStyle.swift
+//  TemperAssessment
+//
+//  Created by Said Rehouni on 5/21/17.
+//  Copyright © 2017 Said Rehouni. All rights reserved.
+//
+
+import UIKit
+
+open class JobListStyle: NSObject {
+
+}
---
+++ TemperAssessment/JobsList/JobsListViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  JobsListViewModel.swift
+//  TemperAssessment
+//
+//  Created by Said Rehouni on 5/21/17.
+//  Copyright © 2017 Said Rehouni. All rights reserved.
+//
+
+import UIKit
+
+open class JobsListViewModel: NSObject {
+
+}
